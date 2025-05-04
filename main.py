5d02e4be2faf28d66c647da7c4e8fa54111f5a10
PavelBartos295 <bartos.pa295@gmail.com>
2025-05-04 22:54:49 +0200

    initial commit | + - / *

---
+++ main.py
@@ -0,0 +1,15 @@
+
+def func_sum(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def div(x, y):
+    if y == 0:
+        return 0
+    else:
+        return x // y
+
+def mult(x, y):
+    return x * y
