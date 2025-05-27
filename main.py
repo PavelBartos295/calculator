cc86995666967093019830ed341ce0036d9f0f6c
PavelBartos295 <bartos.pa295@gmail.com>
2025-05-27 22:40:01 +0200

    Added mathematical operation support for user input

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 
-def func_sum(x, y):
+def manual_sum(x, y):
     return x + y
 
-def sub(x, y):
+def manual_sub(x, y):
     return x - y
 
-def div(x, y):
+def manual_div(x, y):
     if y == 0:
         return 0
     else:
         return x // y
 
-def mult(x, y):
+def manual_mul(x, y):
     return x * y
+
+data = input("Your mathematical problem: ")
+split_data = data.split(" ")
+
+x = int(split_data[0])
+y = int(split_data[2])
+
+if split_data[1] == "+":
+    print(manual_sum(x, y))
+elif split_data[1] == "-":
+    print(manual_sub(x, y))
+elif split_data[1] == "/":
+    print(manual_div(x, y))
+elif split_data[1] == "*":
+    print(manual_mul(x, y))
+else:
+    print("Operation is not supported")
